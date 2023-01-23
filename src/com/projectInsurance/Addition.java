db8c65ca86cacb65f6998180f2b6ffd5f5175be7
Akanksha <akankshakhair123456@gmail.com>
2023-01-22 17:37:43 -0800

    add

---
+++ src/com/projectInsurance/Addition.java
@@ -0,0 +1,14 @@
+package com.projectInsurance;
+
+public class Addition {
+
+			public static void main(String[] args) {
+				int a=10;
+				int b=9;
+				int sum = a+b;
+				System.out.println(sum);
+
+			}
+	}
+
+
